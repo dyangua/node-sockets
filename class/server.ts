ebfb9783cc4cd8afa6685cdd89c7623d0097958e
dyangua <dannyyanguacorrea@hotmail.com>
2019-10-17 22:14:54 -0500

    add socket io

--- class/server.ts
+++ class/server.ts
@@ -1,19 +1,36 @@
 import express from 'express';
 import {SERVER_PORT} from "../global/enviroments";
+import socketIO from 'socket.io'
+import http from 'http'
 
-export default  class Server {
-    public app:express.Application;
-    public port:number;
+export default class Server {
+    private static _instance: Server;
+    public app: express.Application;
+    public port: number;
+    public io: socketIO.Server;
+    private readonly httpServer: http.Server;
 
     constructor() {
         this.app = express();
         this.port = SERVER_PORT;
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer)
+        this.listenSockets();
     }
 
+    private listenSockets() {
+        console.log('Escuchando conexiones');
+        this.io.on('connection', client => {
+            console.log('cliente conectado')
+        });
+    }
 
-    start( callback:any ) {
-        this.app.listen( this.port, callback );
+    public static get instance() {
+        return this._instance || (this._instance = new Server())
+    }
 
+    start(callback: any) {
+        this.httpServer.listen(this.port, callback);
     }
 
 }
--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
-import Server from "./class/server";
 import {router} from "./routes/routes";
 import bodyParser from "body-parser";
+import Server from "./class/server";
 import cors from 'cors';
 
-const server  = new Server();
-server.app.use(bodyParser.urlencoded({extended:true}));
+const server = Server.instance;
+server.app.use(bodyParser.urlencoded({extended: true}));
 server.app.use(bodyParser.json())
 // cors
-server.app.use(cors({origin:true, credentials:true}));
+server.app.use(cors({origin: true, credentials: true}));
 // routes
 server.app.use('/', router);
 
